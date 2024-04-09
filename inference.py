14bfb2228c8561bcae46a53e32f06d3ef10dd1f5
Савелий <113251478+Goolissimo@users.noreply.github.com>
2024-04-09 14:03:01 +0500

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,13 +1,21 @@
 from ultralytics import YOLO
 
-# Загружаем нашу модель
+# Загрузка модели
 model = YOLO("../best.pt")
+#model.to('cuda') #переключение на GPU, если доступно
 
-# Run inference on an image
-results = model("../test_image.png", show=False)  # results list
-print(results[0].boxes.cls)
+# Пути к изображениям
+img1 = '../dataset/happy_whale_dolphins/test_images/0a0a640daf4634.jpg'
+img2 = '../dataset/NDD/BELOW/7.jpg'
+img3 = '../dataset/happy_whale_dolphins/test_images/31a4c316bdb3ba.jpg'
+img4 = '../dataset/NDD/ABOVE/1787.jpg'
+img5 = '../dataset/NDD/ABOVE/1929.jpg'
 
-# View results
-#for r in results:
-#    print(r.boxes)  # print the Boxes object containing the detection bounding boxes
-#r.show()
+# Получение предсказаний
+results = model([img1, img2, img3, img4, img5])  # Возвращает список с предсказаниями
+
+# Отображение результатов
+for idx, result in enumerate(results):
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    result.show()  # display to screen
+#    result.save(filename=f'C:/Users/Лера/Pictures/Saved Pictures/result{idx}.jpg')  # Сохранение на диск
