bee57785c82e585b61ee7a665ac65aa9bd928381
Савелий <113251478+Goolissimo@users.noreply.github.com>
2024-05-18 14:39:27 +0500

    Create train.py

---
+++ scripts/train.py
@@ -0,0 +1,19 @@
+from ultralytics import YOLO
+
+# Загрузка модели
+model = YOLO("yolov8s.pt")  # load a pretrained model
+#Переключение на GPU
+model.to('cuda')
+
+# Обучение модели
+model.train(data='../data.yaml', 
+            epochs=100, 
+            patience=10, 
+            optimizer='auto',
+            imgsz=640,
+            batch=-1,
+            amp=False, 
+            plots=True)
+
+#Оценка качества на валидации
+metrics = model.val()
